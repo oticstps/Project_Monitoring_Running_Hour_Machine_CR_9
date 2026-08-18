1131c796547387eea29c5ab06b4b0381b63e62b6
oticstps <oticsjetson@gmail.com>
2026-08-18 09:50:08 +0700

    Create create_tabel.sql

---
+++ Database/create_tabel.sql
@@ -0,0 +1,239 @@
+CREATE TABLE `rh_imc_21` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_20` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_19` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_103` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_94` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_61` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_251` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_23` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_45` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_229` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+
+CREATE TABLE `rh_imc_21_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_20_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_19_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_103_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_94_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_61_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_251_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_23_trouble` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `rh_imc_21_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_20_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_19_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_103_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+
+CREATE TABLE `rh_imc_94_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_61_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_251_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rh_imc_23_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_45_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `rh_imc_229_run` (
+  `idPrimary` INT(11) NOT NULL AUTO_INCREMENT,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `name_line` VARCHAR(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` VARCHAR(50) COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`idPrimary`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
